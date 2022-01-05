0d426d0ef12da6168daf90096a425dd21cd5d4f6
laviate <valentin.vas.dimitrov@gmail.com>
2022-01-05 22:51:21 +0200

    Added more files

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from rango import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('trading/', views.ivailo_pavlov, name='ivailop')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
-	return HttpResponse("<a href='/rango/about/'>Rango says hey there partner!</a>")
+    return HttpResponse("Rango says hey there partner! <br> <a href='/rango/about/'>About</a>")
+
 
 def about(request):
-	return HttpResponse("<a href='/rango/'>Rango says here is the about page.</a>")
+    return HttpResponse("Rango says here is the about page. <br> <a href='/rango/'>Index</a>")
+
+
+def ivailo_pavlov(request):
+    return HttpResponse("Ивайло обича големи черни пишки<br>  "
+                        "<img src='https://pasteimg.com/images/2022/01/04/ivailo.jpg' alt='Ivcho obicha patki'> ")
